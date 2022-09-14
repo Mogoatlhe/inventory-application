0f9ec6e421913bfb89e682b0955543ba28eee199
mogoatlhe <iam@ofent.se>
2022-09-14 20:54:56 +0200

    retrieve category details

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,5 +1,23 @@
 const Item = require("../models/item");
 
-exports.index = (req, res) => {
-  res.render("index", { title: "Home" });
+exports.index = async (req, res) => {
+  const categoryDetails = await Item.aggregate([
+    {
+      $lookup: {
+        from: "categories",
+        localField: "category",
+        foreignField: "_id",
+        as: "array",
+      },
+    },
+    {
+      $group: {
+        _id: "$category",
+        total: { $sum: 1 },
+        name: { $first: "$array.name" },
+      },
+    },
+  ]);
+
+  res.render("index", { title: "Home", categoryDetails: categoryDetails });
 };
