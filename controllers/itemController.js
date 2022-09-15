0368efc78032ac58f3d874711ea404a64daf98f7
mogoatlhe <iam@ofent.se>
2022-09-15 23:56:06 +0200

    retrieve items from the database

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -2,29 +2,43 @@ const Item = require("../models/item");
 const async = require("async");
 
 exports.index = (req, res, next) => {
-  Item.aggregate(
-    [
-      {
-        $lookup: {
-          from: "categories",
-          localField: "category",
-          foreignField: "_id",
-          as: "array",
-        },
+  async.series(
+    {
+      category_details(callback) {
+        Item.aggregate(
+          [
+            {
+              $lookup: {
+                from: "categories",
+                localField: "category",
+                foreignField: "_id",
+                as: "array",
+              },
+            },
+            {
+              $group: {
+                _id: "$category",
+                total: { $sum: 1 },
+                name: { $first: "$array.name" },
+              },
+            },
+          ],
+          callback
+        );
       },
-      {
-        $group: {
-          _id: "$category",
-          total: { $sum: 1 },
-          name: { $first: "$array.name" },
-        },
+      items(callback) {
+        Item.find({}, callback);
       },
-    ],
+    },
     (err, results) => {
       if (err) {
         return next(err);
       }
-      res.render("index", { title: "Home", categoryDetails: results });
+      res.render("index", {
+        title: "Home",
+        categoryDetails: results.category_details,
+        items: results.items,
+      });
     }
   );
 };
