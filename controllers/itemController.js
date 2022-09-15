3414c36ebbcbe8d15ded628793b652f32f6ee526
mogoatlhe <iam@ofent.se>
2022-09-15 08:17:12 +0200

    retrieve categories asynchronously

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,23 +1,30 @@
 const Item = require("../models/item");
+const async = require("async");
 
-exports.index = async (req, res) => {
-  const categoryDetails = await Item.aggregate([
-    {
-      $lookup: {
-        from: "categories",
-        localField: "category",
-        foreignField: "_id",
-        as: "array",
+exports.index = (req, res, next) => {
+  Item.aggregate(
+    [
+      {
+        $lookup: {
+          from: "categories",
+          localField: "category",
+          foreignField: "_id",
+          as: "array",
+        },
       },
-    },
-    {
-      $group: {
-        _id: "$category",
-        total: { $sum: 1 },
-        name: { $first: "$array.name" },
+      {
+        $group: {
+          _id: "$category",
+          total: { $sum: 1 },
+          name: { $first: "$array.name" },
+        },
       },
-    },
-  ]);
-
-  res.render("index", { title: "Home", categoryDetails: categoryDetails });
+    ],
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      res.render("index", { title: "Home", categoryDetails: results });
+    }
+  );
 };
