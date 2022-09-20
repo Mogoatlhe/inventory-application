bd1519ee32dd53fc040aec9fd92cb12c3db8d3ca
mogoatlhe <iam@ofent.se>
2022-09-20 13:41:19 +0200

    make items page responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./views/*.pug"],
   theme: {
     fontFamily: {
       lato: ["Lato", "sans-serif"],
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      smd: "824px",
+      mmd: "840px",
+      ...defaultTheme.screens,
     },
     extend: {},
   },
