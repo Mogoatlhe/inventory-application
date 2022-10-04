082be2486a3bd6def4765a01c08c8051dd66848b
mogoatlhe <iam@ofent.se>
2022-10-04 19:15:55 +0200

    enable scroll on categories filter hide

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -8,19 +8,25 @@ const closeFilterBtn = document.querySelector(".close-filter-btn");
 const categoriesFilter = document.querySelector(".categories-filter");
 const openFilterBtn = document.querySelector(".open-filter-btn");
 
-closeFilterBtn.addEventListener("click", () => {
+const hideCategoriesFilter = () => {
   categoriesFilter.classList.add("hidden");
+  htmlTag.classList.remove("m-0", "h-full", "overflow-hidden");
+  bodyTag.classList.remove("m-0", "h-full", "overflow-hidden");
+};
+
+closeFilterBtn.addEventListener("click", () => {
+  hideCategoriesFilter();
 });
 
 categoriesFilter.addEventListener("click", function (e) {
   if (e.target !== this) {
     return;
   }
-  categoriesFilter.classList.add("hidden");
+  hideCategoriesFilter();
 });
 
 openFilterBtn.addEventListener("click", () => {
   categoriesFilter.classList.remove("hidden");
-  htmlTag.classList.add("m-[0]", "h-full", "overflow-hidden");
-  bodyTag.classList.add("m-[0]", "h-full", "overflow-hidden");
+  htmlTag.classList.add("m-0", "h-full", "overflow-hidden");
+  bodyTag.classList.add("m-0", "h-full", "overflow-hidden");
 });
