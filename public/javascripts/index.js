df2827e4c0efb868f7445aa3bef33fd720a90d5d
mogoatlhe <iam@ofent.se>
2022-10-04 19:13:15 +0200

    prevent scroll on categories filter display

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,9 @@
+const htmlTag = document.querySelector("html");
+const bodyTag = document.querySelector("body");
+
+htmlTag.classList.remove("m-0", "h-full", "overflow-hidden");
+bodyTag.classList.remove("m-0", "h-full", "overflow-hidden");
+
 const closeFilterBtn = document.querySelector(".close-filter-btn");
 const categoriesFilter = document.querySelector(".categories-filter");
 const openFilterBtn = document.querySelector(".open-filter-btn");
@@ -15,4 +21,6 @@ categoriesFilter.addEventListener("click", function (e) {
 
 openFilterBtn.addEventListener("click", () => {
   categoriesFilter.classList.remove("hidden");
+  htmlTag.classList.add("m-[0]", "h-full", "overflow-hidden");
+  bodyTag.classList.add("m-[0]", "h-full", "overflow-hidden");
 });
