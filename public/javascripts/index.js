e19ca6f4f8324f24302dd32e83a8b76685296b6f
mogoatlhe <iam@ofent.se>
2022-10-04 19:49:09 +0200

    toggle sort menu visibility

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -4,9 +4,12 @@ const bodyTag = document.querySelector("body");
 htmlTag.classList.remove("m-0", "h-full", "overflow-hidden");
 bodyTag.classList.remove("m-0", "h-full", "overflow-hidden");
 
+const openFilterBtn = document.querySelector(".open-filter-btn");
 const closeFilterBtn = document.querySelector(".close-filter-btn");
 const categoriesFilter = document.querySelector(".categories-filter");
-const openFilterBtn = document.querySelector(".open-filter-btn");
+const closeSortBtn = document.querySelector(".close-sort-btn");
+const itemsSort = document.querySelector(".items-sort");
+const openSortBtn = document.querySelector(".open-sort-btn");
 
 const hideCategoriesFilter = () => {
   categoriesFilter.classList.add("hidden");
@@ -29,4 +32,19 @@ openFilterBtn.addEventListener("click", () => {
   categoriesFilter.classList.remove("hidden");
   htmlTag.classList.add("m-0", "h-full", "overflow-hidden");
   bodyTag.classList.add("m-0", "h-full", "overflow-hidden");
+});
+
+closeSortBtn.addEventListener("click", () => {
+  itemsSort.classList.add("hidden");
+});
+
+openSortBtn.addEventListener("click", () => {
+  itemsSort.classList.remove("hidden");
+});
+
+itemsSort.addEventListener("click", function (e) {
+  if (e.target !== this) {
+    return;
+  }
+  itemsSort.classList.add("hidden");
 });
