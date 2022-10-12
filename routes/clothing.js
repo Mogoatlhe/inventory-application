c673375aa83d1a97881c9631a268fed20982d8e8
mogoatlhe <iam@ofent.se>
2022-10-12 10:24:45 +0200

    display images on home page for items with images

--- routes/clothing.js
+++ routes/clothing.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const multer = require("multer");
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, __dirname + "./../public/");
+    cb(null, __dirname + "./../public/images");
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + "." + file.originalname.split(".")[1]); //Appending extension
